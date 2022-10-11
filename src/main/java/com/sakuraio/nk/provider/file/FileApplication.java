f236deb044f9184f22ff7f51f0336b6821329201
nekoimi <nekoimime@gmail.com>
2022-10-11 13:30:57 +0800

    update

--- src/main/java/com/sakuraio/nk/provider/file/FileApplication.java
+++ src/main/java/com/sakuraio/nk/provider/file/FileApplication.java
@@ -1,16 +1,11 @@
 package com.sakuraio.nk.provider.file;
 
-import com.sakuraio.nk.core.holder.SpringContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.servlet.Filter;
 
 /**
  * <p>FileApplication</p>
@@ -25,8 +20,5 @@ public class FileApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(FileApplication.class).bannerMode(Banner.Mode.OFF).run(args);
-        SpringContextHolder.instance().getBeansOfType(FilterRegistrationBean.class).forEach((k, f) -> {
-            log.debug("Filter: {} -> {}", k, f);
-        });
     }
 }
